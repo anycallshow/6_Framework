bfac13c679a8bdf00ce89776147e78e689d4a379
anycallshow <anycallshow@naver.com>
2024-03-12 17:53:53 +0900

    no message

--- 파이널/js/subjectList.js
+++ 파이널/js/subjectList.js
@@ -33,3 +33,27 @@ searchFrm.addEventListener("submit", e => {
     }
 
 })
+
+// 과목 수정 버튼 클릭시
+const table = document.querySelector("table");
+const rowList = table.rows;
+
+for(i=1; i<rowList.length; i++){ // thead부분 제외.
+
+    const row = rowList[i];
+    const updateBtn = row.cells[5];
+
+    updateBtn.addEventListener("click", ()=>{
+        const number = row.cells[0].innerHTML; // 과목 번호
+        const department = row.cells[1].innerHTML; // 학과명
+        const courseName = row.cells[2].innerHTML; // 과목명
+        const professor = row.cells[3].innerHTML; // 전담 교수
+        const lectureTime = row.cells[4].innerHTML; // 강의 시간
+
+        // const str = lectureTime.split('(');
+
+        
+
+    })
+
+}
